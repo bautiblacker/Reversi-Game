2e140134c3b77c5342f201977825789b0b3bcd5c
Martin Craviotto <mcraviotto@itba.edu.ar>
2018-11-02 13:26:20 -0300

    implemented frontEnd

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,16 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Reversi v1.0");
+        primaryStage.setScene(new Scene(root, 680, 600));
         primaryStage.show();
+
     }
 
 
